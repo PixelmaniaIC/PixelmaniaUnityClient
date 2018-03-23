8195db7f85a2b89bd89f4628a7207d582a1dd4b0
Artur Abdalimov <a.abdalimov.97@gmail.com>
2018-03-23 17:01:30 +0600

    Add average color square fill

--- Assets/ImageManager.cs
+++ Assets/ImageManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Assets;
 using UnityEngine;
@@ -10,25 +11,30 @@ public class ImageManager : MonoBehaviour
 
     public int ImageWidth = 128;
     public int ImageHeight = 128;
-    
-    private List<Transform> _squares;
+    public float ImageXOffset;
+    public float ImageYOffset;
+    private List<ImageSquare> _squares;
 
     private void Start()
     {
+        ImageXOffset = ImageWidth / 100f;
+        ImageYOffset = ImageHeight / 100f;
+        
         CreateSquares();
         var www = new WWW("http://res.cloudinary.com/df0xbva5c/image/upload/v1521716516/randevu.png");
-        
+    
+        // TODO: Replace with Coroutine
         while (!www.isDone)
         {
-            
         }
 
         SetTexture(www.texture);
+        ApplyColor(Color.red, 1);
     }
 
     private void CreateSquares()
     {
-        _squares = new List<Transform>();
+        _squares = new List<ImageSquare>();
 
         var posX = -1f;
         var posY = 2f;
@@ -40,12 +46,14 @@ public class ImageManager : MonoBehaviour
             {
                 var obj = Instantiate(ImageSquare, new Vector3(posX, posY, 0), Quaternion.identity);
                 obj.parent = this.transform;
-                obj.GetComponent<ImageSquare>().Index = 4 * i + j;
-                _squares.Add(obj);
-                posX += 1.28f;
+                
+                var square = obj.GetComponent<ImageSquare>();
+                square.Index = 4 * i + j;
+                _squares.Add(square);
+                
+                posX += ImageXOffset;
             }
-
-            posY -= 1.28f;
+            posY -= ImageYOffset;
         }
     }
 
@@ -55,16 +63,41 @@ public class ImageManager : MonoBehaviour
         {
             for (var j = 0; j < 4; j++)
             {
-                var colorBlock = texture.GetPixels(ImageWidth * j, ImageHeight * i, 128, ImageHeight);
+                var squareNum = (12 + j) - i * 4;
+                var square = _squares[squareNum];
+                
+                var colorBlock = texture.GetPixels(ImageWidth * j, ImageHeight * i, ImageWidth, ImageHeight);
                 var appliedTexture = new Texture2D(ImageWidth, ImageHeight);
+                
                 appliedTexture.SetPixels(colorBlock);
                 appliedTexture.Apply();
-
-                _squares[(12 + j) - i * 4].GetComponent<ImageSquare>().texture =
-                    appliedTexture;
-//                    Sprite.Create(t, new Rect(0, 0, 128, 128), new Vector2(0.5f, 0.5f));
+                
+                var averageR = colorBlock.Average(x => x.r);
+                var averageG = colorBlock.Average(x => x.g);
+                var averageB = colorBlock.Average(x => x.b);
+                Debug.Log("R = " + averageR + " G = " + averageG + " B = " + averageB);
+                
+                square.Texture = appliedTexture;
+                square.ForegroundColor = new Color(averageR, averageG, averageB);
+                square.UpdateForgroundColor();
             }
         }
+    }
+
+    public void ApplyColor(Color color, int index)
+    {
+        var texture = _squares[index].Texture;
+
+        for (var i = 0; i < texture.width; i++)
+        {
+            for (var j = 0; j < texture.height; j++)
+            {
+                var current = texture.GetPixel(i, j);
+                var newColor = new Color((current.r + color.r) / 2, (current.g + color.g) / 2, (current.b + color.b) / 2);
+                texture.SetPixel(i, j, newColor);
+            }
+        }
+        texture.Apply();
     }
 
 }
--- Assets/Scripts/ImageSquare.cs
+++ Assets/Scripts/ImageSquare.cs
@@ -1,32 +1,78 @@
-﻿using UnityEngine;
+﻿using Messages;
+using UnityEngine;
 
 namespace Assets
 {
-    public class ImageSquare : MonoBehaviour
+    public class ImageSquare : MonoBehaviour, IReceivable
     {
-        public int width;
-        public int height;
-
-        private bool _isClosed;
-        private Color _foregroundColor;
         public int Index;
-        public Texture2D texture;
+        public Texture2D Texture;
+        public Color ForegroundColor;
 
-        public ImageSquare(int index, int posX, int posY) 
-        {
-            Index = index;
-        }
+        public int Width = 128;
+        public int Height = 128;
+        
+        private bool _isClosed;
+        private SpriteRenderer _spriteRenderer;
 
         private void Awake()
         {
-            var initTexture = new Texture2D(128, 128);
-            GetComponent<SpriteRenderer>().sprite = Sprite.Create(initTexture, new Rect(0, 0, 128, 128), new Vector2(0.5f, 0.5f));
+            _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            var initTexture = new Texture2D(Width, Height);
+            _spriteRenderer.sprite = Sprite.Create(initTexture, new Rect(0, 0, Width, Height), new Vector2(0.5f, 0.5f));
         }
 
         private void OnMouseDown()
         {
-            GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, new Rect(0, 0, 128, 128),
+            _spriteRenderer.sprite = Sprite.Create(Texture, new Rect(0, 0, Width, Height),
                 new Vector2(0.5f, 0.5f));
+            
+            // TODO REMOVE THIS SHIT
+            ApplyReceivedColor(Color.gray);
+        }
+
+        public void UpdateForgroundColor()
+        {
+            var texture = new Texture2D(Width, Height);
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Height; j++)
+                {
+                    texture.SetPixel(i, j, ForegroundColor);
+                }
+            } 
+            texture.Apply();
+            _spriteRenderer.sprite = Sprite.Create(texture, new Rect(0, 0, Width, Height),
+                new Vector2(0.5f, 0.5f));
+        }
+
+        public void ApplyReceivedColor(Color color)
+        {
+            var r = ForegroundColor.r - color.r;
+            var g = ForegroundColor.g - color.g;
+            var b = ForegroundColor.b - color.b;
+            
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Height; j++)
+                {
+                    var current = Texture.GetPixel(i, j);
+                    var updatedColor = new Color(current.r - r, current.g - g, current.b - b);
+                    Texture.SetPixel(i, j, updatedColor);
+                }
+            }
+            
+            Texture.Apply();
+        }
+
+        public string NetworkName
+        {
+            get { return "ImageManager"; }
+        }
+
+        public void ReceiveMessage(Message message)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
