2badcd6db8b2860d3962feb6603d0667add8f85c
Dana <ibraimova.dana@gmail.com>
2018-03-31 19:14:57 +0600

    Имена не выводятся

--- Assets/Scripts/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/Leaderboard/Leaderboard.cs
@@ -2,27 +2,40 @@
 
 public class Leaderboard : IdReceiveListener {
 
+    public LeaderboardDrawer leaderboardDrawer;
+
     private List<User> _users = new List<User>();
 
     // Initializing current user. It should be in separate class
     public override void OnIdReceived()
     {
         var currentUser = new User(PlayerId.instance.id, TcpUnityClient.instance.name, 0, true);
-        AddUser(currentUser);
+        AddUser(currentUser);        
     }
 
     public void AddUser(User user)
     {
         _users.Add(user);
+
+        leaderboardDrawer.UpdateLeaderboard(_users);
     }
 
     public void OfflineUser(string id)
     {
         _users.Find(x => x.id == id).online = false;
+
+        leaderboardDrawer.UpdateLeaderboard(_users);
     }
 
     public void UpdateScore(string id, int score)
     {
         _users.Find(x => x.id == id).score += score;
+
+        leaderboardDrawer.UpdateLeaderboard(_users);
+    }
+
+    public List<User> GetUsers()
+    {
+        return _users;
     }
 }
--- Assets/Scripts/LeaderboardDrawer.cs
+++ Assets/Scripts/LeaderboardDrawer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
+using UnityEngine.UI;
+using System.Linq;
 
 public class LeaderboardDrawer : MonoBehaviour {
 
@@ -9,22 +11,44 @@ public class LeaderboardDrawer : MonoBehaviour {
     public GameObject cube;
     public GameObject colorPicker;
     public DefaultTrackableEventHandler trackableHandler;
+    public List<Text> playerNames;
+    public Leaderboard leaderboardController;
+
+    void Start()
+    {
+        List<User> allUsers = leaderboardController.GetUsers().OrderByDescending(o => o.score).ToList();
+        List<User> leaders = allUsers.GetRange(0, playerNames.Count);
+
+        for (int i = 0; i < playerNames.Count; i++)
+        {
+            playerNames[i].text = leaders[i].name;
+        }
+    }
 
     public void HideOrShow() {
         if (leaderboardPanel.activeInHierarchy)
         {
             leaderboardPanel.SetActive(false);
             if (!trackableHandler.trackingFound) 
-            {
-            //    cube.SetActive(true);
+            {       
                 colorPicker.SetActive(true);
             }            
         }
         else
         {
-            leaderboardPanel.SetActive(true);
-            // cube.SetActive(false);
+            leaderboardPanel.SetActive(true);           
             colorPicker.SetActive(false);
         }		 
-	}
+	}   
+
+    public void UpdateLeaderboard(List<User> allUsers)
+    {
+        List<User> sortedUsers = allUsers.OrderByDescending(o => o.score).ToList();
+        List<User> leaders = sortedUsers.GetRange(0, playerNames.Count);
+
+         for (int i = 0; i < playerNames.Count; i++)
+         {
+             playerNames[i].text = leaders[i].name;
+         }
+    }
 }
