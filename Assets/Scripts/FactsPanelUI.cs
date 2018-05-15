fec47be86c54fe635bdcb47ce4e141b9a05e9362
Dana <ibraimova.dana@gmail.com>
2018-05-15 22:57:21 +0600

    Facts UI

---
+++ Assets/Scripts/FactsPanelUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactsPanelUI : MonoBehaviour {
+
+    public GameObject factsPanel;
+
+    public void Close()
+    {
+        factsPanel.SetActive(false);
+    }
+
+    public void OpenURL()
+    {        
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
